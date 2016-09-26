bde1e081b79425e9647620d884e2635a420ac075
JeremiahLewis0710 <jeremiahlewis@JeremiahLewisMBP.local>
2016-09-25 22:13:34 -0400

    Background styling has been added

--- weatherApp/weather.js
+++ weatherApp/weather.js
@@ -17,21 +17,25 @@ $(function () {
             var darkskycondition = "https://api.darksky.net/forecast/25c7e5437826048f2515b96faa565698/" + latitude + "," + longitude;
             $.ajax(darkskycondition, { dataType: "jsonp" }).done(function (data) {
                 console.log(data);
-                var div = $("<div class = 'card col-sm-3'><button class= 'btn btn-default removeCard' type = 'button'>Remove</button></div>");
+                var div = $("<div class = 'card col-sm-3'></div>");
                 div.append("<h2>" + city + ' ' + state + "</h2>")
                     .append("<hr/>")
-                    .append("<h3>" + data.currently.summary + "</h3>")
-                    .append("Current Chance of Rain is at " + data.currently.precipProbability)
+                    .append("<h3>" + data.currently.summary +"</h3>")
+                    .append("Current Chance of Rain is at " + data.currently.precipProbability+"%")
                     .append("<hr/>")
-                    .append("<h4>" + data.currently.temperature + "</h4>")
-                    .append("The high for today is " + data.daily.data[0].temperatureMax)
+                    .append("<h4>" + parseInt(data.currently.temperature) + "</h4>")
+                    .append("The high for today is " + parseInt(data.daily.data[0].temperatureMax))
                     .append("<br/>")
-                    .append("The low for today is " + data.daily.data[0].temperatureMin)
+                    .append("The low for today is " + parseInt(data.daily.data[0].temperatureMin))
+                    .append("<br/>")
+                    .append("<button class= 'btn btn-default removeCard' type = 'button'>Remove</button>")
                 $(".row").append(div);
 
 
                 $(".removeCard").click(function () {
                     $(this).parent().remove();
+
+
                 });
 
             });
@@ -40,7 +44,45 @@ $(function () {
 
     });
 
+    
 });
+
+
+//  function iconCase(img) {
+//             var weatherImage = data.currently.icon('clear-day');
+           
+//                 switch (img) {
+//                     case "partly-cloudy-day":
+//                         weatherImage("partly-cloudy-day.jpg");
+//                         break;
+//                     case "clear-night":
+//                         weatherImage("clear-night.jpg");
+//                         break;
+//                     case "partly-cloudy-night":
+//                         weatherImage("partly-cloudy-night.jpg");
+//                         break;
+//                     case "cloudy":
+//                         weatherImage("cloudy.jpg");
+//                         break;
+//                     case "rain":
+//                         weatherImage("rain.jpg");
+//                         break;
+//                     case "sleet":
+//                         weatherImage("sleet.jpg");
+//                         break;
+//                     case "snow":
+//                         weatherImage("snow.jpg");
+//                         break;
+//                     case "wind":
+//                         weatherImage("wind.jpg");
+//                         break;
+//                     case "fog":
+//                         weatherImage("fog.jpg");
+//                         break;
+//                     default:
+//                         weatherImage("clear-day.jpg");
+//                         break;
+//                 }
 
 // $(".removeCard").click(function () {
 //     $(".card").remove();
